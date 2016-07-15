afac64a04d2bb5bc6e03002ba2d9201d89d0e8f2
swoozeki <ka.sujit@gmail.com>
2016-07-15 16:14:45 +0000

    homepage route

--- server.js
+++ server.js
@@ -3,4 +3,8 @@ var mongodb=require('mongodb').MongoClient
 
 var app=express()
 
+app.get(function(req,res){
+    res.end("here's your response!")
+})
+
 app.listen(8080,()=>console.log("listening on port 8080"))
